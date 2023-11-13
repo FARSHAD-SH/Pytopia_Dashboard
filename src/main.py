626f2700f8c7c9f93c3974b574b41f956bba25e7
FARSHAD-SH <farshad.shamlu@gmail.com>
2023-11-13 17:54:12 +0100

    Init Streamlit

---
+++ src/main.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.title(":zap: Pytopia Dashboard")
+st.write(":chicken: Welcome to the Pytopia Dashboard! This is a work in progress, so please be patient with us.") 
+st.text("This is a text widget")
